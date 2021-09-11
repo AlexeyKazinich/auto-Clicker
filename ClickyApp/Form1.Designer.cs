6cba555b8bc68d2e43db5292b0573e95bdba9daa
AlexeyKazinich <alexey.kazinich@gmail.com>
2021-09-10 21:00:35 -0400

    added the auto-clicker
    a full version of a working auto-clicker

---
+++ ClickyApp/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace ClickyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBoxRandom = new System.Windows.Forms.CheckBox();
+            this.labelState = new System.Windows.Forms.Label();
+            this.setMax = new System.Windows.Forms.Button();
+            this.labelMax = new System.Windows.Forms.Label();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 182);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Enable";
+            this.toolTip1.SetToolTip(this.checkBox1, "Click to enable the clicker. Wont start clicking until\r\nKeybind is pressed. ON wi" +
+        "ll show when the clicker \r\nis clicking.");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(118, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Set";
+            this.toolTip1.SetToolTip(this.button1, "Click Set to confirm the new desired interval value\r\n");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Interval (ms)";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0",
+            "-",
+            "=",
+            "[",
+            "]",
+            ";",
+            "\'",
+            ",",
+            ".",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(118, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "setKey";
+            this.toolTip1.SetToolTip(this.button2, "Click setKey to confirm the new selected key");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // checkBoxRandom
+            // 
+            this.checkBoxRandom.AutoSize = true;
+            this.checkBoxRandom.Location = new System.Drawing.Point(12, 205);
+            this.checkBoxRandom.Name = "checkBoxRandom";
+            this.checkBoxRandom.Size = new System.Drawing.Size(140, 17);
+            this.checkBoxRandom.TabIndex = 6;
+            this.checkBoxRandom.Text = "Enable Random Interval";
+            this.toolTip1.SetToolTip(this.checkBoxRandom, "Allows to generate random inputs using a max value\r\nand a min value given by the " +
+        "user");
+            this.checkBoxRandom.UseVisualStyleBackColor = true;
+            this.checkBoxRandom.CheckedChanged += new System.EventHandler(this.checkBoxRandom_CheckedChanged);
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.BackColor = System.Drawing.Color.Black;
+            this.labelState.ForeColor = System.Drawing.Color.Red;
+            this.labelState.Location = new System.Drawing.Point(77, 183);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(27, 13);
+            this.labelState.TabIndex = 10;
+            this.labelState.Text = "OFF";
+            this.toolTip1.SetToolTip(this.labelState, "Shows if you are clicking");
+            // 
+            // setMax
+            // 
+            this.setMax.Location = new System.Drawing.Point(382, 45);
+            this.setMax.Name = "setMax";
+            this.setMax.Size = new System.Drawing.Size(75, 23);
+            this.setMax.TabIndex = 7;
+            this.setMax.Text = "setMax";
+            this.setMax.UseVisualStyleBackColor = true;
+            this.setMax.Visible = false;
+            // 
+            // labelMax
+            // 
+            this.labelMax.AutoSize = true;
+            this.labelMax.Location = new System.Drawing.Point(309, 26);
+            this.labelMax.Name = "labelMax";
+            this.labelMax.Size = new System.Drawing.Size(86, 13);
+            this.labelMax.TabIndex = 8;
+            this.labelMax.Text = "Max interval (ms)";
+            this.labelMax.Visible = false;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(276, 47);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMax.TabIndex = 9;
+            this.textBoxMax.Visible = false;
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versionLabel.Location = new System.Drawing.Point(409, 290);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(48, 16);
+            this.versionLabel.TabIndex = 11;
+            this.versionLabel.Text = "v1.0.0";
+            this.versionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 306);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.labelMax);
+            this.Controls.Add(this.setMax);
+            this.Controls.Add(this.checkBoxRandom);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ClickyApp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.CheckBox checkBoxRandom;
+        private System.Windows.Forms.Button setMax;
+        private System.Windows.Forms.Label labelMax;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.Label versionLabel;
+    }
+}
+
---
+++ ClickyApp/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickyApp
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+
+        private const int LEFTUP = 0x0004;
+        private const int LEFTDOWN = 0x0002;
+        public int interval = 100;
+        public bool Click = false;
+        public int parsedValue;
+        public Keys clickKey;
+        private bool released = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread AC = new Thread(AutoClick);
+            backgroundWorker1.RunWorkerAsync();
+            versionLabel.Text = String.Format("v{0}",System.Reflection.Assembly.GetExecutingAssembly().GetName().Version).Substring(0,6);
+
+            AC.Start();
+
+        }
+
+
+        private int getRandomInterval()
+        {
+            int minInt = int.Parse(textBox1.Text);
+            int maxInt = int.Parse(textBoxMax.Text);
+            int returnVal;
+
+            Random rnd = new Random();
+
+            returnVal = rnd.Next(minInt, maxInt);
+
+            return returnVal;
+        }
+
+        private void AutoClick()
+        {
+            while (true)
+            {
+                if (Click == true)
+                {
+                    if (checkBoxRandom.Checked == false)
+                    {
+                        mouse_event(LEFTUP, 0, 0, 0, 0);
+                        Thread.Sleep(1);
+                        mouse_event(LEFTDOWN, 0, 0, 0, 0);
+                        Thread.Sleep(interval);
+                    }
+
+                    if (checkBoxRandom.Checked == true)
+                    {
+                        mouse_event(LEFTUP, 0, 0, 0, 0);
+                        Thread.Sleep(1);
+                        mouse_event(LEFTDOWN, 0, 0, 0, 0);
+                        Thread.Sleep(getRandomInterval());
+                    }
+                }
+                Thread.Sleep(2);
+
+
+
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    if ((GetAsyncKeyState(clickKey) < 0) && released == true && Click == true)
+                    {
+                        Click = false;
+                        this.released = false;
+
+                        //change text color
+                        changeStateColor();
+
+                    }
+                    if ((GetAsyncKeyState(clickKey) < 0) && released == true && Click == false)
+                    {
+                        Click = true;
+                        this.released = false;
+                        changeStateColor();
+                    }
+
+                    if (GetAsyncKeyState(clickKey) >= 0)
+                    {
+                        this.released = true;
+                    }
+
+                    Thread.Sleep(1);
+
+
+
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out parsedValue))
+            {
+                MessageBox.Show("Please enter an Integer");
+                return;
+            }
+            else
+            {
+                interval = int.Parse(textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            #region switch commands
+            switch (comboBox1.Text)
+            {
+                case "A":
+                    clickKey = Keys.A;
+                    break;
+                case "B":
+                    clickKey = Keys.B;
+                    break;
+                case "C":
+                    clickKey = Keys.C;
+                    break;
+                case "D":
+                    clickKey = Keys.D;
+                    break;
+                case "E":
+                    clickKey = Keys.E;
+                    break;
+                case "F":
+                    clickKey = Keys.F;
+                    break;
+                case "G":
+                    clickKey = Keys.G;
+                    break;
+                case "H":
+                    clickKey = Keys.H;
+                    break;
+                case "I":
+                    clickKey = Keys.I;
+                    break;
+                case "J":
+                    clickKey = Keys.J;
+                    break;
+                case "K":
+                    clickKey = Keys.K;
+                    break;
+                case "L":
+                    clickKey = Keys.L;
+                    break;
+                case "M":
+                    clickKey = Keys.M;
+                    break;
+                case "N":
+                    clickKey = Keys.N;
+                    break;
+                case "O":
+                    clickKey = Keys.O;
+                    break;
+                case "P":
+                    clickKey = Keys.P;
+                    break;
+                case "Q":
+                    clickKey = Keys.Q;
+                    break;
+                case "R":
+                    clickKey = Keys.R;
+                    break;
+                case "S":
+                    clickKey = Keys.S;
+                    break;
+                case "T":
+                    clickKey = Keys.T;
+                    break;
+                case "U":
+                    clickKey = Keys.U;
+                    break;
+                case "V":
+                    clickKey = Keys.V;
+                    break;
+                case "W":
+                    clickKey = Keys.W;
+                    break;
+                case "X":
+                    clickKey = Keys.X;
+                    break;
+                case "Y":
+                    clickKey = Keys.Y;
+                    break;
+                case "Z":
+                    clickKey = Keys.Z;
+                    break;
+                case "1":
+                    clickKey = Keys.D1;
+                    break;
+                case "2":
+                    clickKey = Keys.D2;
+                    break;
+                case "3":
+                    clickKey = Keys.D3;
+                    break;
+                case "4":
+                    clickKey = Keys.D4;
+                    break;
+                case "5":
+                    clickKey = Keys.D5;
+                    break;
+                case "6":
+                    clickKey = Keys.D6;
+                    break;
+                case "7":
+                    clickKey = Keys.D7;
+                    break;
+                case "8":
+                    clickKey = Keys.D8;
+                    break;
+                case "9":
+                    clickKey = Keys.D9;
+                    break;
+                case "0":
+                    clickKey = Keys.D0;
+                    break;
+                case "-":
+                    clickKey = Keys.OemMinus;
+                    break;
+                case "=":
+                    clickKey = Keys.Oemplus;
+                    break;
+                case ";":
+                    clickKey = Keys.OemSemicolon;
+                    break;
+                case "'":
+                    clickKey = Keys.OemQuotes;
+                    break;
+                case ",":
+                    clickKey = Keys.Oemcomma;
+                    break;
+                case ".":
+                    clickKey = Keys.OemPeriod;
+                    break;
+                case "/":
+                    clickKey = Keys.OemQuestion;
+                    break;
+                default:
+                    clickKey = Keys.Down;
+                    break;
+
+                    #endregion
+
+            }
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+            Environment.Exit(0);
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+        }
+
+        private void checkBoxRandom_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxRandom.Checked == true)
+            {
+                //show
+                setMax.Visible = true;
+                textBoxMax.Visible = true;
+                labelMax.Visible = true;
+
+                //rename interval(ms) to min interval
+                label1.Text = "Min interval (ms)";
+                button1.Text = "set Min";
+            }
+            else
+            {
+                setMax.Visible = false;
+                textBoxMax.Visible = false;
+                labelMax.Visible = false;
+
+
+                //return the names
+                label1.Text = "interval (ms)";
+                button1.Text = "set Max";
+            }
+        }
+
+
+        private void changeStateColor()
+        {
+
+            if (Click == true)
+            {
+                labelState.Text = "ON";
+                labelState.ForeColor = Color.Lime;
+            }
+
+            else if (Click == false)
+            {
+                labelState.Text = "OFF";
+                labelState.ForeColor = Color.Red;
+            }
+        }
+    }
+}
